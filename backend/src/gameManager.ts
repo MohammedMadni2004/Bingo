17cec297e930694b53d7a4aa6a7ad578d121cd9d
Your Name <you@example.com>
2024-09-24 13:39:55 +0530

    feat:improve code readability

--- backend/src/gameManager.ts
+++ backend/src/gameManager.ts
@@ -1,111 +1,165 @@
-import { Game,player} from "./types";
-import { setTimeout } from "timers/promises";
+import { Game, player } from "./types";
 
 export function handleReset() {
-    const numbers = Array.from({ length: 25 }, (_, i) => i + 1);
-    for (let i = numbers.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-    }
-    const final: number[][] = Array.from({ length: 5 }, (_, i) =>
-      numbers.slice(i * 5, (i + 1) * 5)
-    );
-    return final;
+  const numbers = Array.from({ length: 25 }, (_, i) => i + 1);
+  for (let i = numbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+  }
+  const final: number[][] = Array.from({ length: 5 }, (_, i) =>
+    numbers.slice(i * 5, (i + 1) * 5)
+  );
+  return final;
 };
-export function updateMatrix(matrix:number[][],n:string){
-  let number=parseInt(n);
-  for(let i=0;i<5;i++){
-      for(let j=0;j<5;j++){
-        if(matrix[i][j]===number){
-          matrix[i][j]=0;
-          console.log(matrix[i][j]===number);
-
-        }
-        else if(matrix[i][j]===0){
-          console.log("duplicate move");
-        }
+export function updateMatrix(matrix: number[][], n: string) {
+  let number = parseInt(n);
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      if (matrix[i][j] === number) {
+        matrix[i][j] = 0;
+        console.log(matrix[i][j] === number);
+      }
+      else if (matrix[i][j] === 0) {
+        console.log("duplicate move");
       }
     }
+  }
 };
-export function checkWin(matrix:number[][]){
-  let count=0;
-  for (let i=0;i<5;i++){
-    for(let j=0;j<5;j++){
-      if(matrix[i][j]===0){
-        if(j==4){
+export function checkWin(matrix: number[][]) {
+  let count = 0;
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      if (matrix[i][j] === 0) {
+        if (j == 4) {
           count++;
         }
         continue;
       }
-      else if(matrix[i][j]!=0){
+      else if (matrix[i][j] != 0) {
         break;
       }
     }
   }
-    for (let i=0;i<5;i++){
-      for(let j=0;j<5;j++){
-        if(matrix[j][i]===0){
-          if(j==4){
-            count++;
-          }
-          continue;
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      if (matrix[j][i] === 0) {
+        if (j == 4) {
+          count++;
         }
-        else if(matrix[j][i]!=0){
-          break;
-        }
+        continue;
+      }
+      else if (matrix[j][i] != 0) {
+        break;
       }
     }
-    return count;
+  }
+  return count;
 }
-export function checkDiagonals(matrix:number[][],j:number):number{
-  if(matrix[2][2]==1){
+export function checkDiagonals(matrix: number[][], j: number): number {
+  if (matrix[2][2] == 1) {
     return 0;
   }
-  let temp=j;
- for(let i=0;i<5;i++){
-    if(matrix[i][j]!==0){
-       return 0;
+  let temp = j;
+  for (let i = 0; i < 5; i++) {
+    if (matrix[i][j] !== 0) {
+      return 0;
     }
-    else if(matrix[i][j]==0){
-        if(i==4&&(j==4||j==0)){
-          return 1;
-        }
-        else if(temp==4){
-          j--;
-        }
-        else if(temp==0){
-          j++;
-         }  
-   }
+    else if (matrix[i][j] == 0) {
+      if (i == 4 && (j == 4 || j == 0)) {
+        return 1;
+      }
+      else if (temp == 4) {
+        j--;
+      }
+      else if (temp == 0) {
+        j++;
+      }
+    }
   }
   return 0;
 }
 
-export  function handleInit(game:Game,index:number){
-  if(game.players[0].gameState&&game.players[1].gameState){
-    const firstmat=game.players[0].gameState;
-    const secmat=game.players[1].gameState;
+export function handleInit(game: Game, index: number) {
+  if (game.players[0].gameState && game.players[1].gameState) {
+    const firstmat = game.players[0].gameState;
+    const secmat = game.players[1].gameState;
     game.players[0].Socket.send(`${firstmat}`);
     game.players[1].Socket.send(`${secmat}`);
     game.players[0].Socket.send('true');
-    game.players[0].isPlaying=true;
+    game.players[0].isPlaying = true;
     game.players[1].Socket.send('false');
-    game.players[1].isPlaying=false;
-    
-}
-  else{
-  game.players[index].Socket.send('waiting for other player to start');
+    game.players[1].isPlaying = false;
+    // setTimeout(()=>{
+    //   switchUser(game,0,1);
+    // },5000);
+  }
+  else {
+    game.players[index].Socket.send('waiting for other player to start');
   }
 }
-export function switchUser(game:Game,curP_index:number,otherP_index:number,n?:any){
+export function switchUser(game: Game, curP_index: number, otherP_index: number, n?: any) {
   console.log('switch user called');
-    game.players[curP_index].isPlaying=false;
-    game.players[otherP_index].isPlaying=true;
-    if(n){
+  game.players[curP_index].isPlaying = false;
+  game.players[otherP_index].isPlaying = true;
+  if (n) {
     game.players[otherP_index].Socket.send(`press ${n}`);
-    }
-    game.players[curP_index].Socket.send('false');
-    game.players[otherP_index].Socket.send('true');
+  }
+  game.players[curP_index].Socket.send('false');
+  game.players[otherP_index].Socket.send('true');
   console.log('switch user completed');
-  
+
+}
+export function findPlayer(players: player[], gameManager: Game[], parsed_data): [player, number] | null {
+  const player = players.find((p) => p.id == parsed_data.id);
+  if (player) {
+    console.log("player");
+    const game = gameManager.find((g) => g.gameId === player.gameid);
+    if (game) {
+      console.log("game mila h");
+      let operate_player = game.players.find((p) => p.id === player.id);
+      if (operate_player) {
+        if (game.players[0].id === operate_player.id) {
+          console.log(game.players[0].gameid);
+          return [game.players[0], 0];
+        }
+        else if (game.players[1].id === operate_player.id) {
+          return [game.players[1], 1];
+        }
+      } else {
+        console.log("phir player nahi mila h");
+      }
+    } else {
+      console.log("game nahi mila");
+    }
+  } else {
+    console.log("players wale me nahi mila hai");
+  }
+  return null;
+
+}
+
+export function findGame(gameManager: Game[], Player: player) {
+  const game = gameManager.find((g) => g.gameId === Player.gameid);
+  console.log(Player.gameid);
+  if (game) {
+    return game;
+  }
+  else {
+    console.log()
+    console.log("couldn't find game");
+  }
+}
+export function deleteUsers(players: player[], game: Game, gameManager: Game[]) {
+  let firstIndex = players.indexOf(game.players[0]);
+  let secondIndex = players.indexOf(game.players[1]);
+  if (firstIndex != -1) {
+    players.splice(firstIndex, 1);
+  }
+  if (secondIndex != -1) {
+    players.splice(secondIndex, 1);
+  }
+  let gameIndex = gameManager.indexOf(game);
+  if (gameIndex != -1) {
+    gameManager.splice(gameIndex, 1);
+  }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,14 @@ import bodyParser from "body-parser";
 import WebSocket, { WebSocketServer } from "ws";
 import { player, Game } from "./types";
 import { v4 as uuid } from "uuid";
-import { handleReset } from "./gameManager";
+import { handleReset, findPlayer, findGame, deleteUsers } from "./gameManager";
 import {
   updateMatrix,
   checkWin,
   checkDiagonals,
   handleInit,
   switchUser,
+
 } from "./gameManager";
 import path from "path";
 
@@ -33,34 +34,31 @@ const h_server = app.listen(8080, () => {
 const wss = new WebSocketServer({ server: h_server });
 
 wss.on("connection", function connection(ws: WebSocket) {
-  const playerid = uuid();
+  const playerId = uuid();
   const playerSocket = ws;
-  players.push({ id: playerid, Socket: playerSocket });
-  group.push({ id: playerid, Socket: playerSocket });
+  players.push({ id: playerId, Socket: playerSocket });
+  group.push({ id: playerId, Socket: playerSocket });
   if (group.length === 1) {
-    group[0].Socket.send(`${group[0].id}`);
-  } else if (group.length === 2) {
-    console.log(group[1].id);
-
-    group[1].Socket.send(`${group[1].id}`);
+    group[0].Socket.send(group[0].id);
+  } 
+  if (group.length === 2) {
+    group[1].Socket.send(group[1].id);
     group[0].Socket.send("start");
     group[1].Socket.send("start");
     let gameid = uuid();
+    group[0].gameid = gameid;
+    group[1].gameid = gameid;
     gameManager.push({
       gameId: gameid,
       players: [group[0], group[1]],
       moveCount: 0,
     });
-    console.log(gameManager);
-    //  group[0].Socket.send(`id:${gameid}`);
-    //  group[1].Socket.send(`id:${gameid}`);
-    let player_one = players.find((p) => group[0].id === p.id);
-    let player_two = players.find((p) => group[1].id === p.id);
+    const player_one = players.find((p) => group[0].id === p.id);
+    const player_two = players.find((p) => group[1].id === p.id);
     if (player_one && player_two) {
       player_two.gameid = gameid;
       player_one.gameid = gameid;
-      //player_one.Socket.send(`${gameid}`);
-      //player_two.Socket.send(`${gameid}`);
+      console.log(player_one.gameid, player_two.gameid);
     } else {
       console.log("ok");
     }
@@ -70,189 +68,72 @@ wss.on("connection", function connection(ws: WebSocket) {
   ws.on("message", function message(data: string) {
     const parsed_data = JSON.parse(data);
     if (parsed_data.type == "init") {
-      const player = players.find((p) => p.id === parsed_data.id);
-      if (player) {
-        console.log("player");
-        const game = gameManager.find((g) => g.gameId === player.gameid);
+      const bingo_matrix = handleReset();
+      const result = findPlayer(players, gameManager, parsed_data);
+      if (result) {
+        const [player, index] = result;
+        player.gameState = bingo_matrix;
+        console.log(player.gameid);
+        const game = findGame(gameManager, player);
         if (game) {
-          console.log("game mila h");
-          let mat_player = game.players.find((p) => p.id === player.id);
-          if (mat_player) {
-            console.log("mat_player");
-            //call function here that would generate 2d mattrix)};
-            const bingo_matrix = handleReset();
-            if (game.players[0].id === mat_player.id) {
-              game.players[0].gameState = bingo_matrix;
-              handleInit(game, 0);
-            } else if (game.players[1].id === mat_player.id) {
-              game.players[1].gameState = bingo_matrix;
-              handleInit(game, 1);
-            }
-          }
-          if (!mat_player) {
-            console.log("player nahi mila bsdk");
-          }
+          handleInit(game, index);
         }
-      } else {
-        console.log("dont");
       }
     }
 
-    //This thing is done:we have a player who has sent init message and that guy's matrix must be updated in gameManager array also in the player we have the gameId
-    //now create a random matrix just need to understand little  bit about what it's already done in frontend
-    //also save matrix in the gamemanager
-
     if (parsed_data.type == "move") {
-      //think what will happen if user clicks a number how u can save the state of matrix so that it is easy to check for win
-      //also think how u could manage the switching user every 5-10 seconds
-      //so they r gonna send the player id and then u find appropriate player
-      //along with that they will send a number based on which u need to modify the gamestate of that player
-      //u can make that number to be like 0
-      const player = players.find((p) => p.id === parsed_data.id);
-      if (player) {
-        console.log("player");
-        const game = gameManager.find((g) => g.gameId === player.gameid);
-        if (game) {
-          console.log("game mila h");
-          let operate_player = game.players.find((p) => p.id === player.id);
-          if (operate_player) {
-            if (
-              game.players[0].id === operate_player.id &&
-              game.players[0].gameState
-            ) {
-              //iska matstate update karna h mere biraadar
-              if (game.players[0].isPlaying == true) {
-                console.log("player 0 is elog");
-
-                updateMatrix(game.players[0].gameState, parsed_data.n);
-                game.moveCount = game.moveCount + 1;
-                console.log(game.moveCount);
-              }
-              if ((game.moveCount % 2) + 1 === 2) {
-                console.log("swich user if");
-                switchUser(game, 0, 1, parsed_data.n);
-              } else {
-                game.players[0].Socket.send("true");
-              }
-            } else if (
-              game.players[1].id === operate_player.id &&
-              game.players[1].gameState
-            ) {
-              if (game.players[1].isPlaying == true) {
-                console.log("player 1 is elog");
-                updateMatrix(game.players[1].gameState, parsed_data.n);
-                game.moveCount = game.moveCount + 1;
-                console.log(game.moveCount);
-              }
-              if ((game.moveCount % 2) + 1 == 2) {
-                console.log("swich user if");
-                switchUser(game, 1, 0, parsed_data.n);
-              } else {
-                game.players[1].Socket.send("true");
-              }
+     
+      const result = findPlayer(players, gameManager, parsed_data);
+      if (result) {
+        const [player, index] = result;
+        const game = findGame(gameManager, player);
+        if (player.isPlaying == true && player.gameState) {
+          console.log("player 0 is elog");
+          updateMatrix(player.gameState, parsed_data.n);
+          const other_index = index === 0 ? 1 : 0;
+          if (game) {
+           // setTimeout(()=>{
+            //   switchUser(game,index,other_index);
+            //   game.moveCount=game.moveCount+1;
+            // },8000)
+            game.moveCount = game.moveCount + 1;
+            console.log(game.moveCount);
+            if ((game.moveCount % 2) + 1 == 2) {
+              console.log("swich user if");
+              switchUser(game, index, other_index, parsed_data.n);
+            } else {
+              game.players[index].Socket.send("true");
             }
-          } else {
-            console.log("phir player nahi mila h");
           }
-        } else {
-          console.log("game nahi mila");
         }
-      } else {
-        console.log("players wale me nahi mila hai");
       }
-    } else if (parsed_data.type == "bingo") {
-      //check is it proper
-      const player = players.find((p) => p.id === parsed_data.id);
-      if (player) {
-        console.log("player");
-        const game = gameManager.find((g) => g.gameId === player.gameid);
-        if (game) {
-          console.log("game mila h");
-          let operate_player = game.players.find((p) => p.id === player.id);
-          if (operate_player) {
-            if (
-              game.players[0].id === operate_player.id &&
-              game.players[0].gameState
-            ) {
-              //check weather it state is proper to declare him as winner
-              let rows = checkWin(game.players[0].gameState);
-              let win_diag =
-                checkDiagonals(game.players[0].gameState, 0) +
-                checkDiagonals(game.players[0].gameState, 4);
-              if (rows + win_diag >= 5) {
-                //handle the winner over here
-                game.players[1].Socket.send("other player won");
-                game.players[0].Socket.send("u win");
-                //remove players from gameMnager and players array
-                let firstIndex = players.indexOf(game.players[0]);
-                let secondIndex = players.indexOf(game.players[1]);
-                if (firstIndex != -1) {
-                  players.splice(firstIndex, 1);
-                }
-                if (secondIndex != -1) {
-                  players.splice(secondIndex, 1);
-                }
-                let gameIndex = gameManager.indexOf(game);
-                if (gameIndex != -1) {
-                  gameManager.splice(gameIndex, 1);
-                }
-              } else if (rows + win_diag < 5) {
-                game.players[0].Socket.send("invalid req");
-              }
-            } else if (
-              game.players[1].id === operate_player.id &&
-              game.players[1].gameState
-            ) {
-              //check weather it state is proper to declare him as winner
-              let rows = checkWin(game.players[1].gameState);
-              let win_diag =
-                checkDiagonals(game.players[1].gameState, 0) +
-                checkDiagonals(game.players[1].gameState, 4);
-              if (rows + win_diag >= 5) {
-                //handle the winner over here
-                game.players[0].Socket.send("u lost");
-                game.players[1].Socket.send("u win");
-                let firstIndex = players.indexOf(game.players[0]);
-                let secondIndex = players.indexOf(game.players[1]);
-                if (firstIndex != -1) {
-                  players.splice(firstIndex, 1);
-                }
-                if (secondIndex != -1) {
-                  players.splice(secondIndex, 1);
-                }
-                let gameIndex = gameManager.indexOf(game);
-                if (gameIndex != -1) {
-                  gameManager.splice(gameIndex, 1);
-                }
-              } else if (rows + win_diag < 5) {
-                game.players[1].Socket.send("invalid req");
-              }
+    }
+    else if (parsed_data.type == "bingo") {
+      const result = findPlayer(players, gameManager, parsed_data);
+      if (result) {
+        const [player, index] = result;
+        if (player.gameState) {
+          const rows = checkWin(player.gameState);
+          const win_diag = checkDiagonals(player.gameState, 0) + checkDiagonals(player.gameState, 4);
+          if (rows + win_diag >= 5) {
+            const game = findGame(gameManager, player);
+            if (game) {
+              player.Socket.send('u win');
+              const other_index = index === 0 ? 1 : 0;
+              game.players[other_index].Socket.send('other player won');
+              deleteUsers(players, game, gameManager);
             }
-          } else {
-            console.log("phir player nahi mila h");
           }
-        } else {
-          console.log("game nahi mila");
+          else {
+            player.Socket.send('invalid req')
+          }
         }
-      } else {
-        console.log("players wale me nahi mila hai");
       }
     }
   });
-  // ws.on('disconnection',function disconnection(){
-  // //remove players from gameManager and players array
-  // console.log('hello');
 
-  //   let player= players.find((p)=>{
-  //      return p.Socket===ws;
-  //    });
-  //    if(player){
-  //    console.log("kuch to hua h",player.id);}
-  //    //just remove from gameManager;
-  // });
   wss.on("close", function disconnection(ws) {
     console.log("disco");
   });
 });
 
-//remove players from gameManager and players array
